3fb43370a8baf1a436bfc27cafea657d192df9ef
Sachin <mrmister680@gmail.com>
2024-11-27 12:44:56 +0530

    add ThemeProvider import to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "./provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
