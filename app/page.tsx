9f8b100216a40754b0439031d270af2eb12b54ca
Sachin <mrmister680@gmail.com>
2024-11-27 17:22:23 +0530

    remove unused SVG files and add new Grid component to enhance layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-start flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl">
         <Hero />
+        <Grid/>
       </div>
     </main>
   );
